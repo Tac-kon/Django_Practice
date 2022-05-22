e99b953c826aebb521e46534e02d24352132f6bf
Tac-kon <tkonzx@gmail.com>
2022-05-22 12:37:25 +0900

    Implement post form.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from .models import Post
-
+from .forms import PostForm
 
 def index(request):
     posts = Post.objects.all()
@@ -16,3 +17,14 @@ def detail(request, pk):
         'post': post
     }
     return render(request, 'blog/detail.html', ctx)
+
+def create(request):
+    ctx = {}
+    if request.method == 'GET':
+        ctx['form'] = PostForm
+        return render(request, 'blog/create.html', ctx)
+    elif request.method == 'POST':
+        Post.objects.create(title=request.POST.get('title', None),
+                            content=request.POST.get('content', None),
+                            create_at=timezone.now())
+        return redirect(to='index')
